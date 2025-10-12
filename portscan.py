03baa96c4df611b207bb2defd9cb0c0dfe015a8e
Manoel <63215619+Manoelmonteiro@users.noreply.github.com>
2025-10-12 16:35:12 -0300

    if caso a execucao de errado

--- portscan.py
+++ portscan.py
@@ -1,26 +1,27 @@
 import socket
 from sys import argv, exit
 
-LHOST = "192.168.1.1"
 
-
-def scanPort (alvo, portInitial, portEnd):
+def scan_Port (alvo, portInitial, portEnd):
     for port in range(portInitial, portEnd):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = s.connect_ex ((alvo, port))
         if result == 0:
-            print(f"Port {port} is open")
+            print(f"Porta {port} ta abrida")
         else:
             pass
 
-
-
 if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Uso: python portscan.py <alvo> <porta inicial> <porta final>. porta max 65535")
+        exit()
+    print(argv)
 
     alvo = (argv[1])
     portInitial = int(argv[2])  
     portEnd = int(argv[3])
-    scanPort(alvo, portInitial, portEnd)
+
+    scan_Port(alvo, portInitial, portEnd)
 
 
 
